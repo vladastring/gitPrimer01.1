b30469ef5f7c26f8c9a592b9db1c94a4c50133c9
vladastring <vladastring@gmail.com>
2024-04-12 13:47:52 +0200

    dodat fajl noviFajlMain

---
+++ novaFunkcija.js
@@ -0,0 +1,9 @@
+const proba = (a,b,c) => {
+    if(a > b){
+        return 1;
+    }else if(a < b) {
+        return -1;
+    }
+    return c;
+}
+export default proba;
---
+++ novaFunkcijaMain.js
@@ -0,0 +1,9 @@
+const funkcija2 = () => {
+    console.log("proba 123");
+
+    // neki komentar
+
+    let ime = "Pera";
+
+    return <div>nova funkcija {ime}</div>;
+};
